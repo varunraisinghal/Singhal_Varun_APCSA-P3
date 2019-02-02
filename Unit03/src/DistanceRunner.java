81b50dc7b1079baf800a689cb864f8cd424abbb6
varunsinghal36 <varun.singhalacademic@yahoo.com>
2019-02-02 15:38:45 -0800

    Unit 03 Work

---
+++ Unit03/src/DistanceRunner.java
@@ -0,0 +1,20 @@
+//(c) A+ Computer Science 
+//www.apluscompsci.com
+
+//Name - Varun Singhal
+//Date - 02/02/19
+//Class - AP Comp Sci A
+//Lab  - 3
+
+import java.util.Scanner; 
+import static java.lang.System.*;
+import static java.lang.Math.*;
+
+public class DistanceRunner
+{
+	public static void main( String[] args )
+	{
+		//add test cases	
+			
+	}
+}
