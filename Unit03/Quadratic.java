f4d3f0550c2b15614d6732739f6ae3d74888e729
varunsinghal36 <varun.singhalacademic@yahoo.com>
2019-02-04 12:08:18 -0800

    Adding Unit03 Files

---
+++ Unit03/Quadratic.java
@@ -0,0 +1,45 @@
+//(c) A+ Computer Science
+// www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.util.Scanner; 
+import static java.lang.System.*;
+import static java.lang.Math.*;
+
+public class Quadratic
+{
+	private int a, b, c;
+	private double rootOne;
+	private double rootTwo;
+
+	public Quadratic()
+	{
+	}
+
+	public Quadratic(int quadA, int quadB, int quadC)
+	{
+	}
+
+	public void setEquation(int quadA, int quadB, int quadC)
+	{
+ 	}
+
+	public void calcRoots( )
+	{
+	}
+
+    public void print()
+    {
+    }
+    
+    //complete either print or the toString()
+
+	public String toString()
+	{
+		return "";
+	}
+}
