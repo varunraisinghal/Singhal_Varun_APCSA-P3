f02fd19a9ecc27e7f0eb50a13d1062545efeaa78
varunsinghal36 <varun.singhalacademic@yahoo.com>
2019-02-05 22:17:53 -0800

    Updated AddSubMultRunner.java

--- Unit04/src/AddSubMult.java
+++ Unit04/src/AddSubMult.java
@@ -2,21 +2,29 @@
 //www.apluscompsci.com
 //Name - Varun Singhal
 
-import static java.lang.System.*; 
+import static java.lang.System.*;
+
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class AddSubMult
 {
 	public static double check( double a, double b )
 	{
+		double result = 0;
 		if (a > b)
 		{
-			return 	a - b;
+			result = a - b;
 		}
 		else if (b > a)
 		{
-			return b - a;
+			result = b - a;
 		}
-		return b * a;
+		else
+		{
+			result = b * a;
+		}
+
+		return result;
 	}
 }
