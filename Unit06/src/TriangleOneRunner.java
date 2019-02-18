dfee01b8cce1b11d6314a9f0ded183bcc4270662
varunsinghal36 <varun.singhalacademic@yahoo.com>
2019-02-17 23:40:14 -0800

    Updated Triangle and Triangle Runner

--- Unit06/src/TriangleOneRunner.java
+++ Unit06/src/TriangleOneRunner.java
@@ -4,10 +4,25 @@
 
 import static java.lang.System.*;
 
+import java.util.Scanner;
+
 public class TriangleOneRunner
 {
 	public static void main ( String[] args )
 	{
-		//add test cases		
+		Scanner keyboard = new Scanner(System.in);
+		int x = 0;
+		while (x < 5)
+		{
+			System.out.print("Enter a word :: ");
+			String str = keyboard.nextLine();
+			
+			TriangleOne s = new TriangleOne(str);
+			
+			s.print();
+			System.out.println();
+			x++;
+		}
+
 	}
 }
