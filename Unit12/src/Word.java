ca2a690f5be904e4dad1427bfaf8fb35eb092a47
varunsinghal36 <varun.singhalacademic@yahoo.com>
2019-03-20 12:46:24 -0700

    Finished Word Unit12

--- Unit12/src/Word.java
+++ Unit12/src/Word.java
@@ -7,18 +7,35 @@ import static java.lang.System.*;
 public class Word implements Comparable<Word>
 {
 	private String word;
+	private int length;
 
 	public Word( String s )
 	{
+		word = s;
+		length = s.length();
 	}
 
 	public int compareTo( Word rhs )
 	{		
-		return 0;
+		int sizeA = length;
+		int sizeB = rhs.word.length();
+		
+		if (sizeA > sizeB)
+		{
+			return 1;
+		}
+		else if (sizeB > sizeA)
+		{
+			return -1;
+		}
+		else
+		{
+			return word.compareTo(rhs.word);
+		}
 	}
 
 	public String toString()
 	{
-		return "";
+		return word;
 	}
 }
